5f20063b3df4493562220ac55c5541b4797bb50e
Spada-Michael13 <michaelspada104@gmail.com>
2026-03-21 11:55:43 +0100

    Update query_database_negozio.sql

--- query_database_negozio.sql
+++ query_database_negozio.sql
@@ -1,1 +1,23 @@
+select * 
+from 4CTL_modelli_prodotto
+order by prezzo_listino desc;
 
+select DISTINCT categoria
+from 4CTL_modelli_prodotto;
+
+select *
+from 4CTL_prodotti
+order by data_arrivo
+limit 10;
+
+select cognome
+from 4CTL_clienti
+order by cognome;
+
+  select nome
+from 4CTL_clienti
+order by nome;
+
+select *
+from 4CTL_ordini
+where data_ordine>'2024/05/31' and data_ordine<'2024/07/01';
