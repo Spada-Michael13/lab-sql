88e49d0a37ae437e7b3ddfd8b17144107c325911
Spada-Michael13 <michaelspada104@gmail.com>
2026-03-26 09:05:00 +0100

    Update query_database_negozio.sql

--- query_database_negozio.sql
+++ query_database_negozio.sql
@@ -22,7 +22,14 @@ select *
 from 4CTL_ordini
 where data_ordine>'2024/05/31' and data_ordine<'2024/07/01';
 
-select s.cod_seriale,d.nome from 4CTL_prodotti s ,4CTL_modelli_prodotto d;
+select s.cod_seriale,d.nome 
+from 4CTL_prodotti s ,4CTL_modelli_prodotto d
+where d.id_modello=s.id_modello;
 
-SELECT s.id_ordine,b.cognome,n.nome,c.cod_seriale
-from 4CTL_ordini s,4CTL_clienti b,4CTL_modelli_prodotto n,4CTL_prodotti c;
+SELECT o.id_ordine,c.cognome,m.nome,p.cod_seriale
+from 4CTL_clienti c,4CTL_modelli_prodotto m,4CTL_prodotti p,4CTL_dettagli_ordine d,4CTL_ordini o
+where o.id_cliente=c.id_cliente and m.id_modello=p.id_modello and d.id_ordine=o.id_ordine and d.id_prodotto=p.id_prodotto;
+
+select  g.stato_garanzia,p.cod_seriale
+from 4CTL_garanzie g, 4CTL_prodotti p
+where p.id_prodotto=g.id_prodotto;
